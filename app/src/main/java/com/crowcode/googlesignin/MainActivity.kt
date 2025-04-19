126526f0f58538d38ec8321cabcac3125b6ebf68
emaDRabie <emadrabie046@gmail.com>
2025-04-19 15:30:48 +0200

    updated@main

--- app/src/main/java/com/crowcode/googlesignin/MainActivity.kt
+++ app/src/main/java/com/crowcode/googlesignin/MainActivity.kt
@@ -1,22 +1,30 @@
 package com.crowcode.googlesignin
 
+import android.content.res.Configuration
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.crowcode.googlesignin.ui.theme.GoogleSignInTheme
@@ -29,16 +37,66 @@ class MainActivity : ComponentActivity() {
         setContent {
             GoogleSignInTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        "Emad",
-                        Modifier.padding(innerPadding)
-                    )
+                    CreatingGoogleDesign(Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
+@Composable
+fun CreatingGoogleDesign(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(all = 32.dp)
+    ) {
+        Image(
+            painter = painterResource(R.drawable.google_icon),
+            contentDescription = null,
+            modifier = Modifier
+                .padding(top = 16.dp)
+                .size(50.dp)
+        )
+        Text(
+            text = "Sign In",
+            style = TextStyle(
+                fontSize = 38.sp,
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily(
+                    Font(
+                        R.font.google_font,
+                    )
+                ),
+            ),
+            modifier = Modifier.padding(top = 24.dp)
+        )
+        Text(
+            text = "Use Your Google Account",
+            style = TextStyle(
+                fontSize = 18.sp
+            ),
+            modifier = Modifier
+                .padding(top = 16.dp)
+
+        )
+    }
+}
+
+@Preview(
+    showSystemUi = true,
+    device = "spec:width=411dp,height=891dp"
+)
+@Composable
+fun CreatingGoogleDesignPreview() {
+    CreatingGoogleDesign()
+}
+
+
+/*Greeting(
+    "Emad",
+    Modifier.padding(innerPadding)
+)
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     val localContext = LocalContext.current
@@ -61,4 +119,4 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 
 }
-
+*/
--- app/src/main/java/com/crowcode/googlesignin/ui/theme/Color.kt
+++ app/src/main/java/com/crowcode/googlesignin/ui/theme/Color.kt
@@ -11,5 +11,5 @@ val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
 //val Black = Color(0xFF000000)
-val Green = Color(0xFF4CAF50)
-val White = Color(0xFFFFFFFF)
+//val Green = Color(0xFF4CAF50)
+//val White = Color(0xFFFFFFFF)

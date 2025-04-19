454acfd552a75d415ea75e4c1584d36d14043811
emaDRabie <emadrabie046@gmail.com>
2025-04-19 14:00:33 +0200

    updated@main

--- app/src/main/java/com/crowcode/googlesignin/MainActivity.kt
+++ app/src/main/java/com/crowcode/googlesignin/MainActivity.kt
@@ -14,7 +14,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
@@ -48,7 +47,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         style = TextStyle(
             fontSize = 24.sp,
             fontFamily = FontFamily.Monospace,
-            color = Color(0xFFFFFFFF)
+            color = White
         ),
         modifier = modifier
             .padding(start = 32.dp, top = 64.dp)
